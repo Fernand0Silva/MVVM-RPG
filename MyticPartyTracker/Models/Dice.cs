7bfb60f3e757ebb9d8f926b26a161afa3fee8b61
luiz <luizfernando3995@gmail.com>
2024-04-22 21:33:58 -0300

    MVVM

---
+++ MyticPartyTracker/Models/Dice.cs
@@ -0,0 +1,13 @@
+﻿namespace MyticPartyTracker.Models
+{
+    internal class Dice
+    {
+        public int Side { get; set; }
+
+        public Dice (int side) //
+        {
+            Side = side;
+        }   
+
+    }
+}
---
+++ MyticPartyTracker/View/DiceView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MyticPartyTracker.View;
+
+public partial class DiceView : ContentPage
+{
+	public DiceView()
+	{
+		InitializeComponent();
+	}
+}
